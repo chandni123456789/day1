a3b0d8daa37b6ba7623c59364c059a14597118e5
Chandni kumari <117913760+chandni123456789@users.noreply.github.com>
2022-11-17 16:33:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String[] args)
+  {
+   Scanner s = new Scanner(System.in);
+   System.out.println("addition");
+   System.out.println("enter the numberfirst");
+   int a = s.nextInt();
+   System.out.println("enter the numbersecond");
+    int b = s.nextInt();
+   int sum = a + b;
+   System.out.println("sum of the number is:" + sum);
+   
+
+  }
+}
+   
