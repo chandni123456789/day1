b989b82d37c4b311190c17f2e01135119acea004
Chandni kumari <117913760+chandni123456789@users.noreply.github.com>
2022-11-17 20:09:45 +0530

    Add files via upload

---
+++ MaxMin.java
@@ -0,0 +1,26 @@
+class MaxMin
+{
+   public static void main(String[] args)
+   {
+      System.out.println("max value of integer is:"+Integer.MAX_VALUE);
+      System.out.println("max value of integer is:"+Integer.MIN_VALUE);
+      System.out.println("size of integer is:"+Integer.SIZE);
+
+      System.out.println("max value of float is:"+Float.MAX_VALUE);
+      System.out.println("max value of float is:"+Float.MIN_VALUE);
+      System.out.println("size of float is:"+Float.SIZE);
+
+      System.out.println("max value of byte is:"+Byte.MAX_VALUE);
+      System.out.println("max value of byte is:"+Byte.MIN_VALUE);
+      System.out.println("size of byte is:"+Byte.SIZE);
+     
+      System.out.println("max value of long is:"+Long.MAX_VALUE);
+      System.out.println("max value of long is:"+Long.MIN_VALUE);
+      System.out.println("size of long is:"+Long.SIZE);
+
+      System.out.println("max value of double is:"+Double.MAX_VALUE);
+      System.out.println("max value of double is:"+Double.MIN_VALUE);
+      System.out.println("size of double is:"+Double.SIZE);
+
+    }
+}
